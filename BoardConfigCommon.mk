daeaef486f3344a09d3aa468b27cfd656638f9fc
ghsr <ghsr92@yandex.ru>
2014-12-23 21:41:30 +0500

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,13 @@
 USE_CAMERA_STUB := true
 
+# Board
+TARGET_BOARD_PLATFORM := capri
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := capri
+
+# CPU
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
@@ -7,20 +15,16 @@ TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_SMP := true
 
-TARGET_BOARD_PLATFORM := capri
-TARGET_BOOTLOADER_BOARD_NAME := capri
+# Hardware rendering
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := device/samsung/galaxys2plus-common/configs/egl.cfg
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+BOARD_USE_MHEAP_SCREENSHOT := true
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC
 
-TARGET_NO_BOOTLOADER := true
-
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/samsung/galaxys2plus-common
-BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000
-BOARD_KERNEL_BASE := 0xa2000000
-BOARD_KERNEL_PAGESIZE := 4096
-
-# Filesystem
+# File system
 TARGET_USERIMAGES_USE_EXT4 := true
-
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1610612736
@@ -29,8 +33,26 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 1073741824
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-# Include an expanded selection of fonts
-EXTENDED_FONT_FOOTPRINT := true
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/samsung/galaxys2plus-common
+BOARD_KERNEL_CMDLINE := console=ttyS0,115200n8 mem=832M@0xA2000000 androidboot.console=ttyS0 vc-cma-mem=0/176M@0xCB000000
+BOARD_KERNEL_BASE := 0xa2000000
+BOARD_KERNEL_PAGESIZE := 4096
+
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys2plus-common/recovery/recovery_keys.c
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Hardware
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/galaxys2plus-common/cmhw/
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/samsung/galaxys2plus-common/ril/
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/galaxys2plus-common/include
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -38,6 +60,7 @@ BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/galaxys2plus-common/bluetooth/libbt_vndcfg.txt
 
 # Wi-Fi
+BOARD_NO_WIFI_HAL                   := true
 BOARD_HAVE_SAMSUNG_WIFI             := true
 BOARD_WPA_SUPPLICANT_DRIVER         := NL80211
 WPA_SUPPLICANT_VERSION              := VER_0_8_X
@@ -54,51 +77,30 @@ WIFI_DRIVER_MODULE_NAME             := "dhd"
 WIFI_DRIVER_MODULE_ARG              := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_DRIVER_MODULE_AP_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_apsta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_BAND                           := 802_11_ABG
-BOARD_NO_WIFI_HAL                   := true
 
-# SkTextBox for libtvout
-BOARD_USES_SKTEXTBOX := true
+# healthd
+BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
 
-# Hardware rendering
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/galaxys2plus-common/configs/egl.cfg
-BOARD_USE_MHEAP_SCREENSHOT := true
-BOARD_EGL_WORKAROUND_BUG_10194508 := true
-TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
-COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS -DCAPRI_HWC
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+# BOARD_CHARGER_RES := device/samsung/galaxys2plus-common/res/charger
 
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
 
-# Charger
-BOARD_CHARGER_ENABLE_SUSPEND := true
-BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
-BOARD_CHARGER_RES := device/samsung/galaxys2plus-common/res/charger
-
-# RIL
-BOARD_RIL_CLASS := ../../../device/samsung/galaxys2plus-common/ril/
-
-# Hardware tunables
-BOARD_HARDWARE_CLASS := hardware/samsung/cmhw/ device/samsung/galaxys2plus-common/cmhw/
-
-# GPS
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/galaxys2plus-common/include
+# jemalloc causes a lot of random crash on free()
+MALLOC_IMPL := dlmalloc
 
 # Compat
 TARGET_USES_LOGD := false
 
-# jemalloc causes a lot of random crash on free()
-MALLOC_IMPL := dlmalloc
+# SkTextBox for libtvout
+BOARD_USES_SKTEXTBOX := true
 
-# healthd
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.capri
-
-# Recovery
-BOARD_HAS_NO_SELECT_BUTTON := true
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/galaxys2plus-common/recovery/recovery_keys.c
-TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/class/android_usb/android0/f_mass_storage/lun%d/file"
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += \
