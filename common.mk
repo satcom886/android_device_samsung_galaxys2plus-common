3206c9d82f0014188cc10cfaa7199fd92b98d18d
ghsr <ghsr92@yandex.ru>
2015-02-19 12:14:41 +0600

    galaxys2plus: Remove BUILD_UTC_DATE
     Already defined in CM sources

--- common.mk
+++ common.mk
@@ -162,6 +162,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik heap config
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-
