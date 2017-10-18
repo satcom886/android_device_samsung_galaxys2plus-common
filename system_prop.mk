c39e6a8ad15fc5806fb4fd97e182a275d799db93
satcom886 <rostik.medved@gmail.com>
2017-10-18 16:56:08 +0200

    Update system_prop.mk

--- system_prop.mk
+++ system_prop.mk
@@ -49,5 +49,5 @@ persist.sys.force_highendgfx=true \
 # Other tweaks
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.ota.romname=Lineage OS \
-ro.ota.version=20171012 \
+ro.ota.version=20171812 \
 ro.ota.manifest=https://www.dropbox.com/s/hyxjavnqu0ecn7w/ota_s2vep.xml?dl=1 
